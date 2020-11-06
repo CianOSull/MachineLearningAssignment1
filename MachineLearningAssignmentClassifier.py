b711ee68f659c2bde26e8909ab3fba256cfbf3e9
Cian O'Sullivan <cjp1805@outlook.com>
2020-11-06 22:19:32 +0000

    Updated other classifier file

--- MachineLearningAssignmentClassifier.py
+++ MachineLearningAssignmentClassifier.py
@@ -7,50 +7,22 @@ Created on Mon Nov  2 22:57:32 2020
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import math
+from sklearn import model_selection
+from sklearn import metrics
 
-# This is task 1
-def setup_data():
-    review_df = pd.read_excel("movie_reviews.xlsx")
-    
-    training_data = review_df[review_df['Split'] == "train"][['Review']]
-    
-    training_labels = review_df[review_df['Split'] == "train"][['Sentiment']]
-    
-    test_data = review_df[review_df['Split'] == "test"][['Review']]
-    
-    test_labels = review_df[review_df['Split'] == "test"][['Sentiment']]
-    
-    total_reviews = len(training_labels)
-    
-    total_positive = len(training_labels[training_labels["Sentiment"] == "positive"])
-    total_negative = len(training_labels[training_labels["Sentiment"] == "negative"])
-    
-    print("The number of postive reviews in the training set is >>>: ", 
-          len(training_labels[training_labels["Sentiment"] == "positive"]))
-    
-    print("The number of negative reviews in the training set is >>>: ", 
-          len(training_labels[training_labels["Sentiment"] == "negative"]))
-    
-    print("The number of postive reviews in the evaluation set is >>>: ", 
-          len(test_labels[test_labels["Sentiment"] == "positive"]))
-    
-    print("The number of negative reviews in the evaluation set is >>>: ",
-          len(test_labels[test_labels["Sentiment"] == "negative"]))
-    print(("="*50))
-    
-    return training_data, training_labels, test_data, test_labels, total_positive, total_negative, total_reviews
+def process_text(text):
+    text = "".join(c for c in text if c.isalnum() or c == " ")
+    text = text.lower()
+    text = text.split()
+    return text
 
-# This is task 2
-def create_word_list(training_data, min_word_length, min_word_occ):
+def classifier(feature_data, target_labels, total_positive, total_negative, total_reviews, min_word_length, min_word_occ):
     word_list = []
     word_occurences = {}
     
-    for index, row_value in enumerate(training_data['Review']):
-        transformedValue = "".join(c for c in row_value if c.isalnum() or c == " ")
-        transformedValue = transformedValue.lower()
-        transformedValue = transformedValue.split()
+    for index in range(len(feature_data)):
+        transformedValue = process_text(feature_data.values[index][0])
         
         for word in transformedValue:
             if (min_word_length <= len(word)):
@@ -63,94 +35,102 @@ def create_word_list(training_data, min_word_length, min_word_occ):
         if min_word_occ <= word_occurences[word]:
             word_list.append(word)
             
-    print("Word List size: ", len(word_list))
-    
-    print(("="*50))
-
-    return word_list
-
-# This is task 3
-def count_word_per_reviews(word_list, training_data, training_labels):        
     positive_word_reivew_count = dict.fromkeys(word_list, 0)
     negative_word_reivew_count = dict.fromkeys(word_list, 0)
     
-    for index, row_value in enumerate(training_data['Review']):
-        transformedValue = "".join(c for c in row_value if c.isalnum() or c == " ")
-        transformedValue = transformedValue.lower()
-        transformedValue = transformedValue.split()
+    for index in range(len(feature_data)):
+        transformedValue = process_text(feature_data.values[index][0])
                
         for word in word_list:
-            if word in transformedValue:
-                # Here check if the reivew is ngegative or positive
-                if training_labels.iloc[index].values[0] == "positive": 
-                   positive_word_reivew_count[word] = positive_word_reivew_count[word] + 1
-                   continue
+            if word in transformedValue: 
+                if target_labels.values[index] == 1: 
+                    positive_word_reivew_count[word] = positive_word_reivew_count[word] + 1
+                    continue
                 else:
-                   negative_word_reivew_count[word] = negative_word_reivew_count[word] + 1
-                   continue
-                    
-    
-    print("Task 3 done")
-    print(("="*50))
-    
-    return positive_word_reivew_count, negative_word_reivew_count
+                    negative_word_reivew_count[word] = negative_word_reivew_count[word] + 1
+                    continue
 
-# This is task 4
-def calc_liklihoods(positive_word_reivew_count, negative_word_reivew_count, total_positive, total_negative):
-    # The total reviews of course would be just hte two added together
-    total_reviews = total_positive + total_negative
-    
     alpha = 1
     
     likelihood_positive = positive_word_reivew_count.copy()
+    
     for word in positive_word_reivew_count:
         likelihood_positive[word] = (positive_word_reivew_count[word] + alpha)/(total_positive + 2*alpha)
     
     likelihood_negative = positive_word_reivew_count.copy()
     for word in negative_word_reivew_count:
         likelihood_negative[word] = (negative_word_reivew_count[word] + alpha)/(total_negative + 2*alpha)
-     
-    print("Likelihood positive word: ", list(positive_word_reivew_count)[0], "Ratio:", likelihood_positive[list(positive_word_reivew_count)[0]])
-    print("Negative positive word: ", list(negative_word_reivew_count)[0], "Ratio:", likelihood_negative[list(negative_word_reivew_count)[0]])
     
     prior_review_pos = total_positive/total_reviews
     prior_review_neg = total_negative/total_reviews
     
-    print("Prior Positive Ratio: ", prior_review_pos)
-    print("Prior Negative Ratio: ", prior_review_neg)
+    prediction = []
+    for index in range(len(feature_data)):
+        logLikelihood_positive = 0
+        logLikelihood_negative = 0
+        
+        transformedValue = process_text(feature_data.values[index][0])
+        
+        for word in transformedValue:
+            if word in word_list:
+                logLikelihood_positive = logLikelihood_positive + math.log(likelihood_positive[word])
+                logLikelihood_negative = logLikelihood_negative + math.log(likelihood_negative[word])
+        
+        if math.log(prior_review_pos) - math.log(prior_review_neg) < logLikelihood_positive - logLikelihood_negative:
+            prediction.append(1)
+        else:
+            prediction.append(0) 
     
-    print("Task 4 done")
-    print(("="*50))
-    
-    return likelihood_positive, likelihood_negative, prior_review_pos, prior_review_neg
-
-# This is task 5
-def classifier(review_text, likelihood_positive, likelihood_negative, prior_review_pos, prior_review_neg):
-    logLikelihood_positive = 0
-    logLikelihood_negative = 0
-    
-    transformedValue = "".join(c for c in review_text if c.isalnum() or c == " ")
-    transformedValue = transformedValue.lower()
-    transformedValue = transformedValue.split()
-    
-    for word in transformedValue:
-        # if itis in liklihood text then it should be in negative as well
-        if word in likelihood_positive:
-            logLikelihood_positive = logLikelihood_positive + math.log(likelihood_positive[word])
-            logLikelihood_negative = logLikelihood_negative + math.log(likelihood_negative[word])
-    
-    if math.log(prior_review_pos) - math.log(prior_review_neg) < logLikelihood_positive - logLikelihood_negative:
-        # This one should be 1 for positive
-        prediction = 1
-    else:
-        # This one should be 0 for negative
-        prediction = 0 
-            
     return prediction
+    
+def task6():
+    kf = model_selection.KFold(n_splits=6, shuffle=True)
+    
+    training_data, training_lables, test_data, test_labels, total_positive, total_negative, total_reviews = task1()
+    
+    all_results = []
+    mean_results = []
+    
+    for i in range(1,11):
+        for train_index, test_index in kf.split(training_data):
+            results = classifier(training_data.iloc[train_index], training_lables.iloc[train_index], total_positive, total_negative, total_reviews, i, 10000)
+            
+            all_results.append(metrics.accuracy_score(results, training_lables.iloc[train_index]))
+            print(("="*50))
+    
+        mean_results.append(np.mean(all_results))
+    
+    print("Mean Results: ", mean_results)
+    highest_min_word_len = mean_results.index(max(mean_results))+1
+    print("highest accuracy Min word length: ", highest_min_word_len)
+    
+    true_positive = []
+    true_negative = []
+    false_postiive = []
+    false_negatives = []
+    
+    test_results = classifier(test_data, test_labels, total_positive, total_negative, total_reviews, highest_min_word_len, 10000)
+    
+    all_results.append(metrics.accuracy_score(test_results, test_labels))
+    
+    C = metrics.confusion_matrix(test_labels, test_results)
+    
+    true_positive.append(C[0,0])
+    true_negative.append(C[1,1])            
+    false_postiive.append(C[1,0])
+    false_negatives.append(C[0,1])
+    
+    print(("="*50))
+
+    print("True positives:", round(np.sum(true_positive)/len(test_data), 5), "%")
+    print("True negatives:", round(np.sum(true_negative)/len(test_data), 5), "%")
+    print("False positives:", round(np.sum(false_postiive)/len(test_data), 5), "%")
+    print("False negatives:", round(np.sum(false_negatives)/len(test_data), 5), "%")
+            
+    print("Test Accuracy: ", np.mean(all_results))
 
 def main():
-    # Do task 6 here
-    pass
+    task6()
     
 print(("="*50), "Main", ("="*50))
 main()
